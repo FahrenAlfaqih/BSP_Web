6092f1ac85a963a00d56bf51d2b05638ee507bc3
ShandikaER <sandika05@gmail.com>
2024-05-07 13:12:42 +0700

    TES

--- app/Exports/SelectedSpdExport.php
+++ app/Exports/SelectedSpdExport.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Exports;
+
 use App\Models\Spd;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
@@ -22,8 +23,9 @@ class SelectedSpdExport implements FromCollection, WithHeadings, WithStyles
     public function collection()
     {
         // Mapping data untuk diekspor
-        $mappedData = $this->selectedSpds->map(function ($spd) {
+        $mappedData = $this->selectedSpds->map(function ($spd, $index) {
             return [
+                'No' => $index + 1, // Nomor berurutan
                 'Nama' => $spd->nama,
                 'Nomor SPD' => $spd->nomor_spd,
                 'Dept' => $spd->dept,
@@ -32,7 +34,6 @@ class SelectedSpdExport implements FromCollection, WithHeadings, WithStyles
                 'PO' => $spd->po,
                 'SES' => $spd->ses,
                 'Biaya DPD' => 'Rp ' . number_format($spd->biaya_dpd, 0, ',', '.'),
-                'Submit Tanggal' => $spd->submit_tgl,
             ];
         });
 
@@ -41,6 +42,7 @@ class SelectedSpdExport implements FromCollection, WithHeadings, WithStyles
 
         // Tambahkan baris total biaya DPD ke data yang akan diekspor
         $mappedData->push([
+            'No' => '', // Nomor kosong untuk total biaya DPD
             'Nama' => 'Total Biaya DPD',
             'Nomor SPD' => '',
             'Dept' => '',
@@ -58,6 +60,7 @@ class SelectedSpdExport implements FromCollection, WithHeadings, WithStyles
     public function headings(): array
     {
         return [
+            'No',
             'Nama',
             'Nomor SPD',
             'Dept',
@@ -73,29 +76,45 @@ class SelectedSpdExport implements FromCollection, WithHeadings, WithStyles
         ];
     }
 
+
     public function styles(Worksheet $sheet)
     {
         // Mengatur latar belakang warna untuk baris judul (baris pertama)
-        $sheet->getStyle('A1:L1')->applyFromArray([
+        $sheet->getStyle('A1:M1')->applyFromArray([
+            'font' => [
+                'bold' => true, // Membuat judul tebal
+                'size' => 14, // Ukuran font 12
+                'name' => 'Calibri', // Jenis font Calibri
+            ],
             'fill' => [
                 'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
-                'startColor' => ['rgb' => 'b7e1cd'], // Warna hijau muda (RGB: 183, 225, 205)
+                'startColor' => ['rgb' => 'a9d08e'], // Warna hijau muda
             ],
         ]);
 
-        // Mengatur border untuk seluruh sel
-        $sheet->getStyle('A1:L' . $sheet->getHighestRow())
+
+        $sheet->getStyle('A1:M' . $sheet->getHighestRow())
             ->applyFromArray([
+                'font' => [
+                    'size' => 13, // Ukuran font 11 untuk seluruh teks
+                    'name' => 'Calibri', // Jenis font Calibri
+                ],
                 'borders' => [
                     'allBorders' => [
-                        'borderStyle' => Border::BORDER_THIN, // Gaya border tipis
-                        'color' => ['rgb' => '000000'], // Warna border (hitam)
+                        'borderStyle' => Border::BORDER_THIN,
+                        'color' => ['rgb' => '000000'],
                     ],
                 ],
             ]);
 
+        // Mengatur lebar kolom otomatis
+        foreach (range('A', 'M') as $col) {
+            $sheet->getColumnDimension($col)->setAutoSize(true);
+        }
+
         // Mengatur warna teks merah untuk total biaya DPD
         $lastRow = $sheet->getHighestRow();
-        $sheet->getStyle('H' . $lastRow)->getFont()->setColor(new Color(Color::COLOR_RED));
+        $sheet->getDefaultRowDimension()->setRowHeight(32.1);
+        $sheet->getStyle('I' . $lastRow)->getFont()->setColor(new Color(Color::COLOR_RED));
     }
 }
