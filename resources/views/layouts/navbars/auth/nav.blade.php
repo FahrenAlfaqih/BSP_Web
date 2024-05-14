cd667a6b2aeb6c7983e9d9f4e402450905e2949b
ShandikaER <sandika05@gmail.com>
2024-05-14 20:37:30 +0700

    Nambah nama di navbar

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -4,14 +4,21 @@
         <nav aria-label="breadcrumb">
             <h6 class="font-weight-bolder mb-0 text-uppercase">{{ str_replace('-', ' ', Request::path()) }}</h6>
         </nav>
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar"> 
-            <ul class="navbar-nav  justify-content-end">
-                <li class="nav-item d-flex align-items-center">
-                    <a href="{{ url('/logout')}}" class="nav-link text-body font-weight-bold px-0">
-                        <i class="fa fa-user me-sm-1"></i>
-                        <span class="d-sm-inline d-none">Sign Out</span>
-                    </a>    
-                </li>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar">
+            <ul class="navbar-nav justify-content-end">
+                @if(Auth::check())
+                    <li class="nav-item d-flex align-items-center">
+                        <span class="nav-link text-body font-weight-bold px-0">
+                            <i class="fa fa-user me-sm-1"></i>
+                            <span class="d-sm-inline d-none">{{ Auth::user()->name }}</span>
+                        </span>
+                    </li>
+                    <li class="nav-item d-flex align-items-center">
+                        <a href="{{ url('/logout') }}" class="nav-link text-body font-weight-bold px-0 ms-3">
+                            <span class="d-sm-inline d-none">Sign Out</span>
+                        </a>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
